f868edff45b2c9e8390fbdb979c348741708b174
Exauce-1015127 <1015127@hr.nl>
2025-11-03 21:32:09 +0100

    FEAT: - New pages for about, contact and home - Created navbar - Created stylesheet - Installed React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import Navbar from './Navbar.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-    return <h1>Test website</h1>
+    return (
+        <>
+            <Navbar/>
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                </Routes>
+            </div>
+        </>
+    )
 }
 export default App
