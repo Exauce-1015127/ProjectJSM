20d100ee64379df2af06dfe329a11f1db17f54e2
Exauce-1015127 <1015127@hr.nl>
2026-01-11 20:49:26 +0100

    Changed navbar title to jsm Imported new image Changed herocard image

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(){
 
     return (
         <nav className="nav">
-            <Link to="/" className="site-title">JSM Adviesbureau</Link>
+            <Link to="/" className="site-title">JSM|</Link>
             <ul>
                 <CustomLink to="/contact">Contact</CustomLink>
             </ul>
