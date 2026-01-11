0df840606ce742f731cea937dcfa34ffc5cb60e6
Exauce-1015127 <1015127@hr.nl>
2026-01-11 18:29:49 +0100

    - Changed HeroCard Title to 'over mij' - Changed subtitle to 'over mij' text - Changed buttonlink to LinkedIN

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,10 +16,17 @@ export default function Home() {
 
         <HeroCard
         image="/test2bannerMB.png"
-        title="Voorbeeld"
-        subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        buttonText="Meer informatie"
-        buttonLink="https://www.google.com"
+        title="Over mij"
+        subtitle="Ik ben een ervaren zelfstandige professional met een sterk ontwikkelde omgevingssensitiviteit en gevoel
+        voor politiek-bestuurlijke belangen. In alles wat ik doe, geloof ik in het vinden van een aanvaardbaar evenwicht.
+        Mijn overtuiging is dat alles met elkaar in verbinding staat. Een mismatch tussen verwachtingen en realiteit kan
+        leiden tot onder andere onderlinge irritaties, wantrouwen, (juridische) strijd en politiek-maatschappelijk onrust.
+        Door mijn opgedane kennis en ervaring aan zowel opdrachtgevers (overheden)- als opdrachtnemerskant (aannemers),
+        ben ik beter in staat de uitdagingen waar beide partijen mee te maken hebben in projecten te doorgronden, de issues
+        te overzien en sneller te anticiperen op mogelijke kansen en risico’s. Ik bied klanten advies op maat bij de aanpak
+        van dilemma’s in de menselijke interacties zonder daarbij de doelstellingen van projecten en/of programma’s uit het oog te verliezen."
+        buttonText="LinkedIN"
+        buttonLink="https://www.linkedin.com/in/micha-beto-msc-2b25515/"
         textColor="text-white"
       />
 
