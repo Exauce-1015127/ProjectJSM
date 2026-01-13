235b348613bd00f74b0e46b02b413838157837ce
Exauce-1015127 <1015127@hr.nl>
2026-01-13 01:25:20 +0100

    Created file (footer.js) to connect Footer to sanity. Changed Footer component to implement new sanity Imported file to index.js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,45 +1,79 @@
-import React from 'react'
-import "../footer.css"
+import React, { useEffect, useState } from "react";
+import { sanityClient } from "../lib/sanity";
+import "../footer.css";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="main-footer">
-                <div className="footer-container">
-                    <div className="footer-row">
-                        {/*column 1 */}
-                        <div className="footer-column">
-                            <h4>Contact</h4>
-                            <ul className="footer-list">
-                                <li>06-22976551</li>
-                                <li>info@jsmadviesbureau.nl</li>
-                                <li>KvK-nr.: 73922781</li>
-                            </ul>
-                        </div>
-                        {/*column 2 */}
-                        <div className="footer-column">
-                            <h4>Social media</h4>
-                            <ul className="footer-list">
-                                <li>
-                                    <a
-                                        href="https://www.linkedin.com/in/micha-beto-msc-2b25515/"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >LinkedIn
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="footer-row">
-                        <div className="footer-column">
-                            <p className="col-small">Copyright &copy; {new Date().getFullYear()} JSM Adviesbureau | Omgevingsmanagement | Stakeholdermanagement | Alle rechten voorbehouden</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    )
-}
+  const [footer, setFooter] = useState(null);
 
-export default Footer
+  useEffect(() => {
+    sanityClient
+      .fetch(`
+        *[_type == "footer"][0]{
+          contactTitle,
+          phone,
+          email,
+          kvk,
+          socialTitle,
+          linkedinUrl,
+          companyName,
+          tagline
+        }
+      `)
+      .then(setFooter)
+      .catch(console.error);
+  }, []);
+
+  if (!footer) return null;
+
+  return (
+    <footer>
+      <div className="main-footer">
+        <div className="footer-container">
+
+          <div className="footer-row">
+            {/* Column 1 */}
+            <div className="footer-column">
+              <h4>{footer.contactTitle}</h4>
+              <ul className="footer-list">
+                {footer.phone && <li>{footer.phone}</li>}
+                {footer.email && <li>{footer.email}</li>}
+                {footer.kvk && <li>KvK-nr.: {footer.kvk}</li>}
+              </ul>
+            </div>
+
+            {/* Column 2 */}
+            <div className="footer-column">
+              <h4>{footer.socialTitle}</h4>
+              <ul className="footer-list">
+                {footer.linkedinUrl && (
+                  <li>
+                    <a
+                      href={footer.linkedinUrl}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      LinkedIn
+                    </a>
+                  </li>
+                )}
+              </ul>
+            </div>
+          </div>
+
+          <div className="footer-row">
+            <div className="footer-column">
+              <p className="col-small">
+                Copyright &copy; {new Date().getFullYear()}{" "}
+                {footer.companyName}
+                {footer.tagline && ` | ${footer.tagline}`} | Alle rechten voorbehouden
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
